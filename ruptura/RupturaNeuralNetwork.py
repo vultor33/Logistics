5b498b163c24f19c3d0771893a914f492332b10d
Vultor <fred.vultor@gmail.com>
2019-03-21 10:48:30 -0300

    validation batch added

--- ruptura/RupturaNeuralNetwork.py
+++ ruptura/RupturaNeuralNetwork.py
@@ -16,29 +16,30 @@ class RupturaNeuralNetwork:
             json_file.write(model_json)
         model.save_weights(self.MODEL_NAME + ".h5")    
 
-    def loadModel(self, Ytrain = []):
+    def loadModel(self, size = []):
         json_file = open(self.MODEL_NAME + '.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         loaded_model = model_from_json(loaded_model_json)
         loaded_model.load_weights(self.MODEL_NAME + ".h5")
-        loaded_model = self.compileModel(loaded_model, Ytrain)
+        loaded_model = self.compileModel(loaded_model, size)
         return loaded_model
     
-    def compileModel(self, model, Ytrain):
-        if len(Ytrain) == 0:
+    def compileModel(self, model, size):
+        if len(size) == 0:
             model.compile(loss = self.__DEFAULT_LOSS,optimizer='adam')
         else:
-            custom_loss = self.getCustomLoss(Ytrain)
+            custom_loss = self.getCustomLoss(size)
             model.compile(loss = custom_loss,optimizer='adam')
         return model
     
-    def crossEntropyWeights(self, Ytrain):
+    def crossEntropyWeights(self, size):
+        batch_size, time_steps = size
         weightVector = [1,1,1,0.1]
         w_array = []
-        for i in range(Ytrain.shape[0]):
+        for i in range(batch_size):
             auxWeight = []
-            for j in range(Ytrain.shape[1]):
+            for j in range(time_steps):
                 auxWeight.append(weightVector)
             w_array.append(auxWeight)
         w_array = tf.convert_to_tensor(np.array(w_array), dtype='float')
